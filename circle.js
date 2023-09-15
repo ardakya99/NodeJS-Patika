48125340a7428efd7728f4cf0de530744bfb99e9
Arda Kaya <ardakya99@hotmail.com>
2023-09-15 14:04:27 +0300

    odev3

--- circle.js
+++ circle.js
@@ -1,11 +1,26 @@
 
-let arguments = process.argv.slice(2);
+// let arguments = process.argv.slice(2);
 
-function calculate (radius){
-    const calculateArea = Math.PI * Math.pow(radius, 2);
-    console.log(
-        `Dairenin alanı: (${parseFloat(calculateArea).toFixed(1)})`
-    );
+// function calculate (radius){
+//     const calculateArea = Math.PI * Math.pow(radius, 2);
+//     console.log(
+//         `Dairenin alanı: (${parseFloat(calculateArea).toFixed(1)})`
+//     );
+// }
+
+// calculate(arguments[0]);
+
+
+function circleArea(r) {
+    let area  = r * r * Math.PI;
+    console.log("Dairenin alanı:",area);
+    // return area
 }
 
-calculate(arguments[0]);
+function circleCircumference(r) {
+    let circumrefence = 2 * r * Math.PI;
+    console.log("Dairenin Çevresi:",circumrefence);
+    // return circumrefence
+}
+
+module.exports = {circleArea, circleCircumference};
